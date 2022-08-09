ff537f067454c04ec413e1d52fbd260b049b72be
miguelvalden <miguelvalden77@gmail.com>
2022-08-09 18:18:20 +0200

    init

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,15 @@
+
+
+
+const IdCard = ({firstName, lastName, gender, height, birth})=>{
+    return(
+        <div>
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Gender: {gender}</p>
+            <p>Height: {height}</p>
+            <p>Birth: {birth}</p>
+        </div>
+        )
+}
+
